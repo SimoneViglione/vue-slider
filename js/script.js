f35e694bf0ab39de836f60f219897830ecaf02a9
Simone Viglione <zimoviglio51@gmail.com>
2023-03-15 17:49:47 +0100

    corretti text e title

--- js/script.js
+++ js/script.js
@@ -7,27 +7,27 @@ const { createApp } = Vue
             {
               image: 'img/01.webp',
               title: 'Spiderman',
-              text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+              text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
             },
             {
               image: 'img/02.webp',
               title: 'Ratchet & Clank',
-              text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+              text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
             },
             {
               image: 'img/03.webp',
               title: 'Fortnite',
-              text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+              text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.'
             },
             {
               image: 'img/04.webp',
               title: 'Stray',
-              text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+              text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
             },
             {
               image: 'img/05.webp',
               title: 'Avengers',
-              text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+              text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay..'
             }
           ],
           active: 0
